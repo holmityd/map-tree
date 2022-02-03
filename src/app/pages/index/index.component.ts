7c51d697fbf2f02f0cde71509d372fff8aec1c53
Nicat <bayramov.nicat@hotmail.com>
2022-02-03 08:46:01 +0400

    fix

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -8,9 +8,8 @@ import abc from 'poe-data/decodeUrl';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
-  // encodedTree = '';
-  encodedTree = 'https://poeskilltree.com/?v=3.17.0-atlas#AAAABAAAAAAbAB8AeQCeALoAzgHAAxQDHwOhBG8EmATWBNsFEwU7BpMHiAfXB-IIGwg_CGwIyQjkCdwJ-AoZCj0KQApXCmEKvgr7CwoLJAtxDOAOsw6-DtMO4g8tD2MPzBCwENwQ4REGEYkRmhJIEsAUdxTaFWEVsRXAFsQXFBeQF8sX7hgbGDkYuxjEGX4ZghtOG2Qc1h10Hh0eWx59HzgfZh_EH_YgKyFrIcUiayPaJMsk6STvJf4mWCaKJuco0CjpKYcqhCrRK0EraCt-K4YruivXK9oseiz8LdAuiy6-LwQvRi9uL5wwAzAOMDUwZjFrMiQySzMgM0kz5DQ9NIg0pDSmNa41zTZdOG85jjpGOq87nz5CPl8-wT80P80_40A8QG1AfkFfQYxCDEITQiVCc0J3QsFDI0NLQ6lEc0SWRJpFo0XhRw1Hz0gASCJILEiwSbxJyknTSdRKDkpfTatN_E5vT11PYU-JT9hQF1A5UYJSO1M4U2FTolP7VYdWRVZGVm1XJFfjWAJYRVibWKdYqVjBWRRZFlk8WcxaNFp9WptayVrOW9lb81yMXNlc3F0TXU9dt13HXipe918YX1Nf62AhYE1gZ2B7YUhiKWJ_YrhjAWO1ZExkjWSyZWtlfWWXZaRmtWbGZspnLmedZ-loQGhSaKFosmi2aR5pm2nVadxqz2sCazZsD2xJbH5skGzMbNRs_W19baVt_W4Gbipvf3AhcEBwUHBkcQtxK3KIcsty3HOLc6Vz4nP4dPp1E3UndUN2RXZUdp12wncUdyx3tXiEeTd5nnnDegF6tnr0ev17CnunfMV8z30TfR19Pn9cf6R_s3_cgGSAeIEPgVqCUYJpgoqDlIPDg92EEYRuhH2FJ4XEhpiH6Yf7iBmIOIhtiKOJMIlCiZGKyYrRixiLLIz9jSmNTo2hjmqOg46Ijx2Pt4_UkKmQ45EykU2RfZGMkcSRx5IEkiySTJKwkteTQJThla2WzZdFl8OY2JkMmTiZbJlzmZGZ6pwrnTidXp2HnbGeSaBdoXihtaHRoeKi3qMZo2yjbqOFo4mjxqQPpDakSqSspSSlXKV4pa6l0KXYpm2mxKbap4KoL6j0qP2qtaq5q22rfKusrBCsGqwfrNmtXK23rdauAq5irueu6q_Br8Wxc7GCsfuyZrMts4y0hrURtWe1fbX9tiq26Lb4tyu3SLeBt8u3zLhKuYC6F7pMurK6yrrvuwm7KrtevKi83L44vme-178Wv0C_ssAZwLrA9cGGwcfC_8NNw6rEScUCxVbF_sYzxjjGm8bQxtnHs8e0yGDIfsjlyaHKH8p6yyvLy8z5znrOg86Rz7fPyc_y0ErQUNCi0KnRJNFN0WPRqdL00zjTXdNe1IfUudTj1UTVZtYt1jDWWtZb1qDW29ce103XVteF2I7Ynti12NDZhdmQ2a3ZuNnn2k3avdrO24fcTdy_3RzdaN713yPfS99e3_jgFuBr4YvhqeHr4nHil-Kn4vXjcON85Izlj-W35ezl-uYm5k_m9Oco51jnlufh6Czo0uja6ObpH-nT6mnqjerK6tPq1et-7EnsTOzl7RPtWe2g7lzulO7B76nvrvFL8d7yZfKT8ujzBPNs8-P0DPVM9Xf1qPYN9lL3T_fk-CH4zvj6-QD5Dvow-mj6svra-vz70Pv5_AT8zvzm_Pb9Hf3J_dH-3g==';
-  data = data;
+  encodedTree = '';
+  // encodedTree = 'https://poeskilltree.com/?v=3.17.0-atlas#AAAABAAAAAAbAB8AeQCeALoAzgHAAxQDHwOhBG8EmATWBNsFEwU7BpMHiAfXB-IIGwg_CGwIyQjkCdwJ-AoZCj0KQApXCmEKvgr7CwoLJAtxDOAOsw6-DtMO4g8tD2MPzBCwENwQ4REGEYkRmhJIEsAUdxTaFWEVsRXAFsQXFBeQF8sX7hgbGDkYuxjEGX4ZghtOG2Qc1h10Hh0eWx59HzgfZh_EH_YgKyFrIcUiayPaJMsk6STvJf4mWCaKJuco0CjpKYcqhCrRK0EraCt-K4YruivXK9oseiz8LdAuiy6-LwQvRi9uL5wwAzAOMDUwZjFrMiQySzMgM0kz5DQ9NIg0pDSmNa41zTZdOG85jjpGOq87nz5CPl8-wT80P80_40A8QG1AfkFfQYxCDEITQiVCc0J3QsFDI0NLQ6lEc0SWRJpFo0XhRw1Hz0gASCJILEiwSbxJyknTSdRKDkpfTatN_E5vT11PYU-JT9hQF1A5UYJSO1M4U2FTolP7VYdWRVZGVm1XJFfjWAJYRVibWKdYqVjBWRRZFlk8WcxaNFp9WptayVrOW9lb81yMXNlc3F0TXU9dt13HXipe918YX1Nf62AhYE1gZ2B7YUhiKWJ_YrhjAWO1ZExkjWSyZWtlfWWXZaRmtWbGZspnLmedZ-loQGhSaKFosmi2aR5pm2nVadxqz2sCazZsD2xJbH5skGzMbNRs_W19baVt_W4Gbipvf3AhcEBwUHBkcQtxK3KIcsty3HOLc6Vz4nP4dPp1E3UndUN2RXZUdp12wncUdyx3tXiEeTd5nnnDegF6tnr0ev17CnunfMV8z30TfR19Pn9cf6R_s3_cgGSAeIEPgVqCUYJpgoqDlIPDg92EEYRuhH2FJ4XEhpiH6Yf7iBmIOIhtiKOJMIlCiZGKyYrRixiLLIz9jSmNTo2hjmqOg46Ijx2Pt4_UkKmQ45EykU2RfZGMkcSRx5IEkiySTJKwkteTQJThla2WzZdFl8OY2JkMmTiZbJlzmZGZ6pwrnTidXp2HnbGeSaBdoXihtaHRoeKi3qMZo2yjbqOFo4mjxqQPpDakSqSspSSlXKV4pa6l0KXYpm2mxKbap4KoL6j0qP2qtaq5q22rfKusrBCsGqwfrNmtXK23rdauAq5irueu6q_Br8Wxc7GCsfuyZrMts4y0hrURtWe1fbX9tiq26Lb4tyu3SLeBt8u3zLhKuYC6F7pMurK6yrrvuwm7KrtevKi83L44vme-178Wv0C_ssAZwLrA9cGGwcfC_8NNw6rEScUCxVbF_sYzxjjGm8bQxtnHs8e0yGDIfsjlyaHKH8p6yyvLy8z5znrOg86Rz7fPyc_y0ErQUNCi0KnRJNFN0WPRqdL00zjTXdNe1IfUudTj1UTVZtYt1jDWWtZb1qDW29ce103XVteF2I7Ynti12NDZhdmQ2a3ZuNnn2k3avdrO24fcTdy_3RzdaN713yPfS99e3_jgFuBr4YvhqeHr4nHil-Kn4vXjcON85Izlj-W35ezl-uYm5k_m9Oco51jnlufh6Czo0uja6ObpH-nT6mnqjerK6tPq1et-7EnsTOzl7RPtWe2g7lzulO7B76nvrvFL8d7yZfKT8ujzBPNs8-P0DPVM9Xf1qPYN9lL3T_fk-CH4zvj6-QD5Dvow-mj6svra-vz70Pv5_AT8zvzm_Pb9Hf3J_dH-3g==';
   tree?: number[];
   nodes?: any[];
   stats?: string[];
@@ -173,8 +172,9 @@ export class IndexComponent implements OnInit {
         name: 'maven',
         keys: ['maven', 'orbs of conflict']
       }
-    ]
+    ];
 
+  // Get tree
   private _findKeysInString(str: string, keys: string[]): boolean {
     return keys.map(item => str.includes(item)).find(item => item) || false;
   }
@@ -204,7 +204,6 @@ export class IndexComponent implements OnInit {
 
     setTimeout(() => this.masonry());
   }
-
   ngOnInit() {
     this.onChange();
     this.categoried.forEach(category => {
@@ -221,23 +220,15 @@ export class IndexComponent implements OnInit {
   }
   masonry(gridGutter = 10, dGridCol = 3, tGridCol = 2, mGridCol = 1) {
     if (!this.grid || !this.gridCell) return;
-    var g = this.grid.nativeElement,
-      gc = this.gridCell.toArray().map(item => item.nativeElement),
-      gcLength = gc.length,
-      gHeight = 0,
-      i;
-
-    for (i = 0; i < gcLength; ++i) {
-      gHeight += gc[i].offsetHeight + gridGutter;
-    }
-
-    if (window.screen.width >= 1024) {
-      g.style.height = gHeight / dGridCol + gHeight / (gcLength + 1) + "px";
-    } else if (window.screen.width < 1024 && window.screen.width >= 768) {
-      g.style.height = gHeight / tGridCol + gHeight / (gcLength + 1) + "px";
-    } else {
-      g.style.height = gHeight / mGridCol + gHeight / (gcLength + 1) + "px";
-    }
+    let gHeight = 0;
+    let dGrids = document.body.clientWidth >= 1024 ?
+      dGridCol :
+      document.body.clientWidth < 1024 && document.body.clientWidth >= 768 ?
+        tGridCol : mGridCol;
+    this.gridCell.toArray().forEach(item => {
+      gHeight += item.nativeElement.offsetHeight + gridGutter;
+    });
+    this.grid.nativeElement.style.height = gHeight / dGrids + gHeight / (this.gridCell.length + 1) + "px";
   }
   @HostListener('window:resize', ['$event'])
   onResize() {
